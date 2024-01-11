ef8a991230e6cd8f94beb4df8bb1860aa255f10a
souta0318 <94553485+souta0318@users.noreply.github.com>
2024-01-12 05:32:24 +0900

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ function loadNavigationBar(relativePath) {
     document.write(`
             <a href="${relativePath}index.html" class="button">Topページへ</a></p>
             <a href="${relativePath}チャンピオンまとめ/LoLチャンピオンまとめ.html" class="button">チャンピオン一覧</a></p>
-            <a href="${relativePath}アイテムまとめ/アイテムまとめ.html" class="button">アイテム一覧</a></p>
+            <a href="${relativePath}アイテムまとめ/アイテムまとめ.html" class="button">アイテム一覧（v14.1変更中）</a></p>
             <a href="${relativePath}ルーン紹介/ルーンまとめ.html" class="button">ルーンについて</a></p>
             <a href="${relativePath}LoL用語解説/LoL用語解説.html" class="button">LoL用語まとめ</a></p>
             <a href="${relativePath}サモナースペル/サモナースペル.html" class="button">サモナースペル説明</a></p>
